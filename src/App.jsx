af14fd045f18b97a0bd42dc97d4160a55000cf95
vesyean <veyseankem@gmail.com>
2026-07-25 09:00:41 +0700

    change route

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,13 @@ const App = () => {
     <div>
       <Navigation />
       <Routes>
-        <Route path='/home' element={<Home/>} />
-        <Route path='/menu' element={<Menu/>} />
-        <Route path='/review' element={<Review/>} />
-        <Route path='/contact' element={<Contact/>} />
+-        <Route path='/' element={<Home/>} />
++        <Route path='/' element={<Navigate to="/home" replace />} />
++        <Route path='/home' element={<Home/>} />
+         <Route path='/menu' element={<Menu/>} />
+         <Route path='/review' element={<Review/>} />
+         <Route path='/contact' element={<Contact/>} />
++        <Route path='*' element={<Navigate to="/home" replace />} />
       </Routes>
       <menu/>
       <Footer/>
